245e133a50326d57ec43146628b377180a9cb485
Nicholas Rempel <nbrempel@gmail.com>
2014-03-01 16:53:17 -0800

    add procfile

--- config/local.py
+++ config/local.py
@@ -5,6 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 CSRF_ENABLED = False
-SECRET_KEY = 'you-will-never-guess'
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db')
