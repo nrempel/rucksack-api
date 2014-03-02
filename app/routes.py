14e47abcb060d9cfa53e231dea87801208288fe9
Nicholas Rempel <nbrempel@gmail.com>
2014-03-01 19:14:06 -0800

    set up some todos

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,12 @@ from app.models import WebComponent
 
 
 @app.route("/")
-def hello():
+def list_components():
     num_components = len(WebComponent.query.all())
     return "Number of records: %d" % num_components
+    # TODO: retrieve, jsonify, and return all records
+
+
+# @app.route("/", methods=['POST'])
+# def insert_component():
+#     TODO: create record
