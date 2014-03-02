8d5a79c8bed6efaff7268087109a8da2f25f3dd1
Nicholas Rempel <nbrempel@gmail.com>
2014-03-01 17:13:27 -0800

    port is an int

--- config/production.py
+++ config/production.py
@@ -3,7 +3,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-PORT = os.environ['PORT']
+PORT = int(os.environ['PORT'])
 DEBUG = False
 CSRF_ENABLED = True
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
