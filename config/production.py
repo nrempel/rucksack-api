b848648185759c7a664e8470e366cc409a770875
Nicholas Rempel <nbrempel@gmail.com>
2014-03-01 17:19:07 -0800

    debugging on heroku

--- config/production.py
+++ config/production.py
@@ -3,6 +3,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+print os.environ['PORT']
+print int(os.environ['PORT'])
+
 PORT = int(os.environ['PORT'])
 DEBUG = False
 CSRF_ENABLED = True
