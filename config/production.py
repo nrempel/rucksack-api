e642fd48c2fedc4c22285dae2e98e93014237eb7
Nicholas Rempel <nbrempel@gmail.com>
2014-03-01 16:59:53 -0800

    remove secret

--- config/production.py
+++ config/production.py
@@ -5,6 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = False
 CSRF_ENABLED = True
-SECRET_KEY = os.environ['FLASK_SECRET_KEY']
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db')
